ff265a7ef76afe710888e436df36f4a3e5cee5f8
claykaufmann <clay.kaufmann@gmail.com>
2021-01-10 18:23:27 -0700

    add react-burger-menu, setup links, delete burger and menu thing

--- components/jsx-components/photo-components/Header/Header.jsx
+++ components/jsx-components/photo-components/Header/Header.jsx
@@ -1,16 +1,39 @@
-import Burger from '../Menu/Burger';
-import Menu from '../Menu/Menu';
-import { useState } from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import Link from 'next/link';
 
 const BaseHeader = () => {
-    const [open, setOpen] = useState(false);
     return (
         <header>
             {/* THIS SHOULD BE A PHOTO AT SOME POINT */}
             <h1>Clay Kaufmann Photography</h1>
-            <Burger open={open} setOpen={setOpen} />
-            <Menu open={open} setOpen={setOpen} />
+            <Menu>
+                <Link href='/'>
+                    <a>Back to main site</a>
+                </Link>
+                <Link href='/photography'>
+                    <h1>Clay Kaufmann</h1>
+                </Link>
+                <Link href='/photography/galleries'>
+                    <a>Galleries</a>
+                </Link>
+                <Link href='/photography/galleries/featured'>
+                    <a>Featured</a>
+                </Link>
+                <Link href='/photography/galleries/landscapes'>
+                    <a>Landscapes</a>
+                </Link>
+                <Link href='/photography/galleries/abstract'>
+                    <a>Abstracts</a>
+                </Link>
+                <Link href='/photography/galleries/blackwhite'>
+                    <a>Black and White</a>
+                </Link>
+                <Link href='/photography/galleries/all'>
+                    <a>All Photos</a>
+                </Link>
+            </Menu>
         </header>
     );
 };
+
 export default BaseHeader;
