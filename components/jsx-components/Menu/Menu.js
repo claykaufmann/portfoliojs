f47a5b283731a50b46c733601ccc52e95cff7562
claykaufmann <clay.kaufmann@gmail.com>
2021-01-22 17:56:08 -0800

    finish base mobile styling, begin styling images with horizontal scroll on larger screens

--- components/jsx-components/Menu/Menu.js
+++ components/jsx-components/Menu/Menu.js
@@ -3,29 +3,64 @@ import Link from 'next/link';
 import { bool } from 'prop-types';
 import styles from './Menu.module.scss';
 
-const Menu = ({ open }) => {
+const Menu = ({ open, setOpen }) => {
     return (
         <nav className={open ? styles.openMenu : styles.closedMenu}>
             <Link href='/' className={styles.menuLink}>
-                <a className={styles.menuLinkText}>Home</a>
+                <a
+                    className={styles.menuLinkText}
+                    onClick={() => setOpen(!open)}
+                >
+                    Home
+                </a>
             </Link>
             <Link href='/about' className={styles.menuLink}>
-                <a className={styles.menuLinkText}>About</a>
+                <a
+                    className={styles.menuLinkText}
+                    onClick={() => setOpen(!open)}
+                >
+                    About
+                </a>
             </Link>
             <Link href='/galleries/featured' className={styles.menuLink}>
-                <a className={styles.menuLinkText}>Featured</a>
+                <a
+                    className={styles.menuLinkText}
+                    onClick={() => setOpen(!open)}
+                >
+                    Featured
+                </a>
             </Link>
             <Link href='/galleries/landscapes' className={styles.menuLink}>
-                <a className={styles.menuLinkText}>Landscapes</a>
+                <a
+                    className={styles.menuLinkText}
+                    onClick={() => setOpen(!open)}
+                >
+                    Landscapes
+                </a>
             </Link>
             <Link href='/galleries/abstract' className={styles.menuLink}>
-                <a className={styles.menuLinkText}>Abstracts</a>
+                <a
+                    className={styles.menuLinkText}
+                    onClick={() => setOpen(!open)}
+                >
+                    Abstracts
+                </a>
             </Link>
             <Link href='/galleries/blackwhite' className={styles.menuLink}>
-                <a className={styles.menuLinkText}>Black / White</a>
+                <a
+                    className={styles.menuLinkText}
+                    onClick={() => setOpen(!open)}
+                >
+                    Black / White
+                </a>
             </Link>
             <Link href='/film' className={styles.menuLink}>
-                <a className={styles.menuLinkText}>Film</a>
+                <a
+                    className={styles.menuLinkText}
+                    onClick={() => setOpen(!open)}
+                >
+                    Film
+                </a>
             </Link>
         </nav>
     );
